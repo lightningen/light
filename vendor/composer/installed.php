37882f301e5cab00d8528174c6ae05f50b026f71
lightningen <lightningen@outlook.com>
2023-12-20 03:24:04 +0800

    Add composer.lock and vendor/

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yourusername/yourrepositoryname',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5fe0239b0e985f162548a3ec48a209cfd063e76',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yourusername/yourrepositoryname' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5fe0239b0e985f162548a3ec48a209cfd063e76',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
